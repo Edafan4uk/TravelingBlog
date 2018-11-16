04154916e59d701eb5560775c6f3a33ee3b866f7
Nazar <nazarst1225@gmail.com>
2018-11-16 12:08:25 +0200

    Change solution structure 2

--- AlexProject/TravelingBlog.BusinessLogicLayer/ViewModels/DTO/RatingDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using TravelingBlog.DataAcceesLayer.Models.Entities;
-
-namespace TravelingBlog.BusinessLogicLayer.ViewModels.DTO
-{
-    public class RatingDTO
-    {
-        public bool? RatingPostBlog { get; set; }
-    }
-}
