091b9b55df3f43c64f51fbcb707bcbd52ff588ce
Oleksandr <sashadanilenko@mail.ua>
2018-11-19 17:04:37 +0200

    Add fluent required

--- TravelingBlog.BusinessLogicLayer/ViewModels/DTO/PostBlogDTO.cs
+++ TravelingBlog.BusinessLogicLayer/ViewModels/DTO/PostBlogDTO.cs
@@ -9,9 +9,7 @@ namespace TravelingBlog.BusinessLogicLayer.ViewModels.DTO
     public class PostBlogDTO
     {
         public int? Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Plot { get; set; }
         public DateTime DateOfCreation { get; set; } = DateTime.Now.Date;
         public int TripId { get; set; }
