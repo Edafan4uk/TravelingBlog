918ba802d20611e35740315ee0efd3a4f6847c7c
Danil <danilmonets@gmail.com>
2018-11-20 18:00:12 +0200

    Update RolesController.cs

--- TravelingBlog/Controllers/RolesController.cs
+++ TravelingBlog/Controllers/RolesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -6,29 +7,29 @@ using Microsoft.AspNetCore.Identity;
 using TravelingBlog.DataAcceesLayer.Models.Entities;
 using TravelingBlog.BusinessLogicLayer.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using TravelingBlog.BusinessLogicLayer.ViewModels.DTO;
+using TravelingBlog.BusinessLogicLayer.Contracts;
+using TravelingBlog.DataAcceesLayer.Models;
 
 namespace TravelingBlog.Controllers
 {
-<<<<<<< HEAD
     //[Authorize]
     [Route("api/[controller]/[action]")]
     //[Authorize(Roles = "moderator, admin")]
-=======
-    //[Authorize(Roles = "moderator")]
-    [Route("api/[controller]")]
->>>>>>> parent of 1c1b02a... Changed RolesController, added RoleDTO, ApplicationRole
     public class RolesController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
         private readonly UserManager<AppUser> userManager;
-        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        private ILoggerManager logger;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, ILoggerManager logger)
         {
             this.roleManager = roleManager;
             this.userManager = userManager;
+            this.logger = logger;
         }
 
-<<<<<<< HEAD
-        
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -37,7 +38,7 @@ namespace TravelingBlog.Controllers
             //try
             //{
             //int IdDTO = 0;
-            
+
             //    foreach (var role in rolesList)
             //    {
             //        Int32.TryParse(role.Id, out IdDTO);
@@ -49,12 +50,9 @@ namespace TravelingBlog.Controllers
             //    logger.LogError($"Wrong input format inside RolesController;{e.Message}");
             //    return StatusCode(500, "Internal Server Error");
             //}
-            
+
             return Ok(rolesList);
         }
-=======
-        public IActionResult Index() => Ok(roleManager.Roles.ToList());
->>>>>>> parent of 1c1b02a... Changed RolesController, added RoleDTO, ApplicationRole
 
         [HttpPost]
         public async Task<IActionResult> Create(string name)
@@ -62,10 +60,7 @@ namespace TravelingBlog.Controllers
             if (!string.IsNullOrEmpty(name))
             {
                 IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
-<<<<<<< HEAD
-                
-=======
->>>>>>> parent of 1c1b02a... Changed RolesController, added RoleDTO, ApplicationRole
+
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index");
@@ -91,8 +86,17 @@ namespace TravelingBlog.Controllers
             }
             return RedirectToAction("Index");
         }
+        //[HttpPost]
+        //public async Task<IActionResult> Delete(string name)
+        //{
+        //    IdentityRole role = await roleManager.FindByNameAsync(name);
+        //    if (role != null)
+        //    {
+        //        IdentityResult result = await roleManager.DeleteAsync(role);
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
-<<<<<<< HEAD
         [HttpGet]
         public IActionResult UserList()
         {
@@ -109,10 +113,8 @@ namespace TravelingBlog.Controllers
             //}
             return Ok(userList);
         }
-=======
-        public IActionResult UserList() => Ok(userManager.Users.ToList());
->>>>>>> parent of 1c1b02a... Changed RolesController, added RoleDTO, ApplicationRole
 
+        [HttpGet]
         public async Task<IActionResult> Edit(string userId)
         {
             AppUser user = await userManager.FindByIdAsync(userId);
@@ -155,5 +157,6 @@ namespace TravelingBlog.Controllers
             }
             return NotFound();
         }
+
     }
 }
